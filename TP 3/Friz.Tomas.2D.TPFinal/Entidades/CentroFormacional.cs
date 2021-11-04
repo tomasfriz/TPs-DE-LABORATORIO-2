a85dc01657d45a92f9348a6cb120151ac3d07326
tomasfriz <tomyagustinf@gmail.com>
2021-11-04 14:32:28 -0300

    Falta terminar

---
+++ TP 3/Friz.Tomas.2D.TPFinal/Entidades/CentroFormacional.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Entidades
+{
+    public class CentroFormacional
+    {
+
+        Datos objd = new Datos();
+        //public DataTable N_login(Persona obje)
+        //{
+        //    return objd.D_login(obje);
+        //}
+
+        //public DataTable N_listar_usuarios()
+        //{
+        //    return objd.D_listarusuarios();
+        //}
+
+        //public DataTable N_buscarusuario(Persona obje)
+        //{
+        //    return objd.D_buscarusuarios(obje);
+        //}
+
+        //public DataTable N_listar_tipo()
+        //{
+        //    return objd.D_listatipo();
+        //}
+
+        //public String N_mantenimientousuario(Persona obje)
+        //{
+        //    return objd.D_mantenimientousuarios(obje);
+        //}
+
+        //public DataTable N_listar_salon()
+        //{
+        //    return objd.D_listasalon();
+        //}
+
+        //public DataTable N_listar_curso()
+        //{
+        //    return objd.D_listacurso();
+        //}
+
+        //public String N_mantenimientosalon(Persona obje)
+        //{
+        //    return objd.D_mantenimientosalon(obje);
+        //}
+        //public String N_mantenimientocurso(Persona obje)
+        //{
+        //    return objd.D_mantenimientocurso(obje);
+        //}
+
+        //public String N_pass(Persona obje)
+        //{
+        //    return objd.D_pass(obje);
+        //}
+
+        //public DataTable N_totalcurso()
+        //{
+        //    return objd.D_totalcursos();
+        //}
+
+        //public DataTable N_totalsalones()
+        //{
+        //    return objd.D_totalsalones();
+        //}
+
+        //public DataTable N_totalalumnos()
+        //{
+        //    return objd.D_totalalumnos();
+        //}
+
+        //public DataTable N_listaralumnos()
+        //{
+        //    return objd.D_listar_alumnos();
+        //}
+
+        //public DataTable N_buscaralumnos(Persona obje)
+        //{
+        //    return objd.D_buscaralumnos(obje);
+        //}
+
+        //public String N_mantenimientoalumno(Persona obje)
+        //{
+        //    return objd.D_mantenimientoalumno(obje);
+        //}
+
+        //public DataTable N_buscaralumnosXcurso(Persona obje)
+        //{
+        //    return objd.D_buscaralumnosXcurso(obje);
+        //}
+        //public DataTable N_buscaralumnosXsalon(Persona obje)
+        //{
+        //    return objd.D_buscaralumnosXsalon(obje);
+        //}
+    }
+}
---
+++ TP 3/Friz.Tomas.2D.TPFinal/TesteoUnitario/Validacion.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+
+namespace TesteoUnitario
+{
+    [TestClass]
+    public class Validacion
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
